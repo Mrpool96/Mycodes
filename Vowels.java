58c223e0d9cf0cdfe46b16b9eec494f3c6998550
Mrpool96 <53513296+Mrpool96@users.noreply.github.com>
2019-08-08 12:32:11 +0530

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -3,8 +3,6 @@ public class Vowels {
     public static void main(String[] args) {
         String str = "BeginnersBook";
         int vcount = 0, ccount = 0;
-
-        //converting all the chars to lowercase
         str = str.toLowerCase();
         for(int i = 0; i < str.length(); i++) { char ch = str.charAt(i); if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') { vcount++; } else if((ch >= 'a'&& ch <= 'z')) {
             ccount++;
